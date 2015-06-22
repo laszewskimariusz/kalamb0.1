410577e24c6ab18865bbf00277530044f9293c14
Mariusz Laszewski <laszewskimariusz@gmail.com>
2015-06-23 00:26:21 +0100

    Porzadki, pozbycie sie irytujacego bledu przy zamykaniu aplikacji

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from pygame import mixer
 import time
 
 Root = Tk()
-RTitle = Root.title("kalambur.01")
+RTitle = Root.title("kalambury 0.1")
 
 Root.geometry("1280x720")
 Root.wm_iconbitmap("icon.ico")
 LARGE_FONT = ("Verdana", 23)
-PKT_FONT =("Verdana", 15)
+PKT_FONT = ("Verdana", 15)
 
 mixer.init(44100)
 
@@ -17,27 +17,9 @@ clock = mixer.Sound("Ticking-clock.wav")
 alarm = mixer.Sound("alarm.wav")
 
 
-class Puntkacja:
-
-
-
-
-
-    def d_niebieska(self):
-        pkt = '10'
-
-        niebieska = Label(Root, text=("niebieska:"), wraplength=300, justify=LEFT, fg='blue', font=PKT_FONT, width=10, height=5)
-        niebieska.grid(column=6, row=1)
-
-
-
-
-
-
-
 class Timer:
 
-    def countDown(self):
+    def count_down(self):
         for czas in range(60, -1, -1):
             sf = "{:02d}:{:02d}".format(*divmod(czas, 120))
             time.sleep(1)
@@ -46,16 +28,15 @@ class Timer:
             if sf == '00:01':
                 alarm.play()
 
+            try:
+                czas_okno = Label(Root, text=sf, wraplength=300, justify=LEFT, bg="blue", fg='ivory',
+                                  font=LARGE_FONT, width=10, height=5)
+                czas_okno.grid(column=2, row=4)
+                Root.update()
 
-            czas_okno = Label(Root, text=(sf), wraplength=300, justify=LEFT, bg="blue", fg='ivory', font=LARGE_FONT, width=10, height=5)
-            czas_okno.grid(column=2, row=5)
+            except:
 
-
-
-
-
-            Root.update()
-
+                Root.mainloop()
 
 
 class Haslo:
@@ -63,45 +44,30 @@ class Haslo:
     def losuj(self):
 
         self.line = random.choice(open('hasla.txt').readlines())
-        okno = Label(Root, text=(self.line),wraplength=300, justify=LEFT, bg="blue", fg='ivory',font=LARGE_FONT, width=30, height=10)
-        okno.grid(column=2, row=2)
+        okno = Label(Root, text=self.line, wraplength=300, justify=LEFT, bg="blue", fg='ivory', font=LARGE_FONT,
+                     width=30, height=10)
+        okno.grid(column=2, row=1)
 
         print(self.line)
 
 
 class MainWindow(Haslo, Timer):
 
-
     def __init__(self, master):
+
         frame = Frame(master)
-        frame.pack
+        frame.grid()
 
         haslo = Haslo()
         haslo.losuj()
 
         odlicz = Timer()
 
-        gracze = Puntkacja()
-        gracze.d_niebieska()
-
-
-
-
-        Label(text='Kalambury by Mariusz ').grid(column=4, row=0)
+        Label(text='Kalambury by Mariusz ').grid(column=3, row=0)
         Label(text='').grid(column=1, row=3, sticky=('n', 's'))
 
-        Button(Root, text="Losuj", height=8, width=15, bg="pink", command=(self.losuj)).grid(column=5, row=2)
-        Button(Root, text="Czas start", height=8, width=15, bg="pink", command=(odlicz.countDown)).grid(column=5, row=5)
-
-
-
-
-
-
-
-
-
-
+        Button(Root, text="Losuj", height=8, width=15, bg="pink", command=self.losuj).grid(column=5, row=1)
+        Button(Root, text="Czas start", height=8, width=15, bg="pink", command=odlicz.count_down).grid(column=5, row=4)
 
 app = MainWindow(Root)
 Root.mainloop()
