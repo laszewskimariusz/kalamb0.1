248132275e2176adc4d6c19ce6417e3632ccb24a
Mariusz Laszewski <laszewskimariusz@gmail.com>
2015-06-25 23:02:13 +0100

    Poprawki, na podstawie pomocy formu python polska

--- run.py
+++ run.py
@@ -1,73 +1,113 @@
-from tkinter import *
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+ 
+try:
+    import Tkinter as tk # Python 2.x
+except:
+    import tkinter as tk # Python 3.x
+ 
 import random
 from pygame import mixer
-import time
-
-Root = Tk()
-RTitle = Root.title("kalambury 0.1")
-
-Root.geometry("1280x720")
-Root.wm_iconbitmap("icon.ico")
+import csv
+ 
+#----------------------------------------------------------------------
+ 
 LARGE_FONT = ("Verdana", 23)
-PKT_FONT = ("Verdana", 15)
+PKT_FONT   = ("Verdana", 15)
+ 
+#----------------------------------------------------------------------
+ 
+class App(tk.Tk):
+ 
+    def __init__(self):
+        tk.Tk.__init__(self)
+ 
+        self.title("kalambury 0.1")
 
-mixer.init(44100)
-
-clock = mixer.Sound("Ticking-clock.wav")
-alarm = mixer.Sound("alarm.wav")
-
-
-class Timer:
-
-    def count_down(self):
-        for czas in range(60, -1, -1):
-            sf = "{:02d}:{:02d}".format(*divmod(czas, 120))
-            time.sleep(1)
-            if sf == '00:13':
-                clock.play()
-            if sf == '00:01':
-                alarm.play()
-
-            try:
-                czas_okno = Label(Root, text=sf, wraplength=300, justify=LEFT, bg="blue", fg='ivory',
-                                  font=LARGE_FONT, width=10, height=5)
-                czas_okno.grid(column=2, row=4)
-                Root.update()
-
-            except:
-
-                Root.mainloop()
-
-
-class Haslo:
-
+        self.wm_iconbitmap("icon.ico")
+ 
+        mixer.init(44100)
+ 
+        self.clock = mixer.Sound("Ticking-clock.wav")
+        self.alarm = mixer.Sound("alarm.wav")
+ 
+        self.timer = None
+ 
+        # --- wczytanie z CSV ---
+ 
+        with open('hasla.txt') as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            self.data = list(reader)
+ 
+        #print self.data
+ 
+        # --- naglowek ---
+ 
+        tk.Label(self, text='Kalambury by Mariusz').grid(column=0, row=0, columnspan=3)
+ 
+        # --- przyciski ---
+ 
+        tk.Button(self, text="Losuj", height=8, width=15, bg="pink",
+                    command=self.losuj).grid(column=0, row=3)
+ 
+        tk.Button(self, text="Czas start", height=8, width=15, bg="pink",
+                    command=lambda:self.countdown(60)).grid(column=2, row=3)
+ 
+        # --- kategoria i haslo ---
+ 
+        self.category = tk.StringVar()
+ 
+        tk.Label(self, textvariable=self.category, wraplength=500, justify=tk.LEFT,
+                    bg="blue", fg='ivory', font=LARGE_FONT,
+                    width=50, height=5).grid(column=0, row=1, columnspan=3)
+ 
+        self.subject = tk.StringVar()
+ 
+        tk.Label(self, textvariable=self.subject, justify=tk.LEFT,
+                    bg="blue", fg='ivory', font=LARGE_FONT,
+                    width=50, height=5).grid(column=0, row=2, columnspan=3)
+ 
+        # --- zegar ---
+ 
+        self.time = tk.StringVar()
+        self.time.set("00:00")
+ 
+        tk.Label(self, textvariable=self.time, justify=tk.LEFT,
+                    bg="blue", fg='ivory', font=LARGE_FONT, width=10,
+                    height=5).grid(column=1, row=3)
+ 
+        # --- inne ---
+ 
+        self.losuj()
+ 
+ 
+    def run(self):
+        self.mainloop()
+ 
+ 
     def losuj(self):
-
-        self.line = random.choice(open('hasla.txt').readlines())
-        okno = Label(Root, text=self.line, wraplength=300, justify=LEFT, bg="blue", fg='ivory', font=LARGE_FONT,
-                     width=30, height=10)
-        okno.grid(column=2, row=1)
-
-        print(self.line)
-
-
-class MainWindow(Haslo, Timer):
-
-    def __init__(self, master):
-
-        frame = Frame(master)
-        frame.grid()
-
-        haslo = Haslo()
-        haslo.losuj()
-
-        odlicz = Timer()
-
-        Label(text='Kalambury by Mariusz ').grid(column=3, row=0)
-        Label(text='').grid(column=1, row=3, sticky=('n', 's'))
-
-        Button(Root, text="Losuj", height=8, width=15, bg="pink", command=self.losuj).grid(column=5, row=1)
-        Button(Root, text="Czas start", height=8, width=15, bg="pink", command=odlicz.count_down).grid(column=5, row=4)
-
-app = MainWindow(Root)
-Root.mainloop()
+        category, subject = random.choice(self.data)
+        self.category.set('Kategoria: ' + category)
+        self.subject.set('Hasło: ' + subject)
+        #print(category, subject)
+ 
+        # zatrzymanie odliczania
+        if self.timer:
+            self.after_cancel(self.timer)
+            self.time.set("00:00")
+ 
+ 
+    def countdown(self, left_time):
+        self.time.set("00:{:02d}".format(left_time))
+ 
+        if left_time == 13:
+            self.clock.play()
+        elif left_time == 1:
+            self.alarm.play()
+ 
+        if left_time > 0:
+            self.timer = self.after(1000, self.countdown, left_time-1)
+ 
+#----------------------------------------------------------------------
+ 
+App().run()
